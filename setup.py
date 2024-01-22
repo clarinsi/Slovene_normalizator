b382b388c156934349faa4cbac5c507ba0a55905
ppisljar <peter.pisljar@gmail.com>
2024-01-22 19:51:59 +0100

    updating

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup, find_packages
+import os
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+requirements = open(os.path.join(cwd, "requirements.txt"), "r").readlines()
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    README = readme_file.read()
 
 setup(
-    name='normalizator',
-    version='0.1',
+    name='slovene_normalizator',
+    version='0.0.1',
+    author = "Peter Pisljar",
+    description = "slovenian text normalizator",
+    #long_description=README,
+    install_requires=requirements,
     packages=find_packages(),
-    install_requires=[
-        # Add any required external dependencies here
-    ],
+    python_requires=">=3.8.0, <3.12",
     include_package_data=True,
 )
